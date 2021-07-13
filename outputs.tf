0c344f580a7cda8fdba057758c40b1c5cca466dc
Dan <dangibbo@amazon.com>
2021-07-13 14:44:12 +0100

    Add bucket output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "s3_state_bucket_name" {
    value = join("", aws_s3_bucket.tf_state_bucket.*.id)
 }
 
+output "s3_state_bucket" {
+  value = aws_s3_bucket.tf_state_bucket.bucket
+}
+
 output "dynamodb_table_arn" {
   description = "ARN of the DynamoDB table"
   value       = element(concat(aws_dynamodb_table.tf_lock_state.*.arn, [""]), 0)
